590dba86732deb96412bdd5f44224e717d26297e
Martin Biermann <martin@biermann.io>
2023-03-16 15:05:12 +0000

    Amending webhook creation with conflict for duplicate endpoint registration under same client ID

--- service/webhook.js
+++ service/webhook.js
@@ -13,11 +13,25 @@ module.exports = {
         if (!("url" in req.body) || req.body.url === null || req.body.url === "" || !/http[s]?\:\/\/.+/.test(req.body.url)) {
             res.status(400).send("Please provide a valid URL beginning with http(s)://.")
         } else {
-            storage.saveWebhookForClientID(req.client_id, req.body.url).then((id) => {
-                res.status(200).json({id: id})
-            }).catch(err => {
-                console.log(`Error creating webhook for client ID ${req.client_id}: ${err}`)
-                res.sendStatus(403)
+            let checkEndpointAlreadyExists = (url, client_id) => {
+                return new Promise((resolve, reject) => {
+                    storage.getWebhooksForClientID(client_id).then(webhooks => {
+                        for (wh of webhooks) {
+                            if (wh.url === url) reject("Conflict: Endpoint already registered for this client ID.")
+                        }
+                        resolve()
+                    })
+                })
+            }
+            checkEndpointAlreadyExists(req.body.url, req.client_id).then(() => {
+                storage.saveWebhookForClientID(req.client_id, req.body.url).then((id) => {
+                    res.status(200).json({id: id})
+                }).catch(err => {
+                    console.log(`Error creating webhook for client ID ${req.client_id}: ${err}`)
+                    res.sendStatus(403)
+                })
+            }).catch(e => {
+                res.status(409).json({error: e})
             })
         }
     },
